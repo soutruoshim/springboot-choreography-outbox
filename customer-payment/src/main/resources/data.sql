fc1ea1b2e0422bb66caaa624ba61f08a691a0325
srhdp <srhdp@BRN-MacBook-Pro.local>
2025-08-18 12:00:17 +0700

    payment service integrate test

--- customer-payment/src/main/resources/data.sql
+++ customer-payment/src/main/resources/data.sql
@@ -1,35 +1,23 @@
-DROP TABLE IF EXISTS order_payment;
-DROP TABLE IF EXISTS order_inventory;
-DROP TABLE IF EXISTS purchase_order;
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS customer_payment;
 
-CREATE TABLE purchase_order (
-                                order_id uuid default random_uuid() primary key,
-                                customer_id int,
-                                product_id int,
-                                quantity int,
-                                unit_price int,
-                                amount int,
-                                status VARCHAR(50),
-                                delivery_date TIMESTAMP,
-                                version int
+CREATE TABLE customer (
+                          id int AUTO_INCREMENT primary key,
+                          name VARCHAR(50) NOT NULL,
+                          balance int
 );
 
-CREATE TABLE order_payment (
-                               id int AUTO_INCREMENT primary key,
-                               order_id uuid unique,
-                               payment_id uuid,
-                               success boolean,
-                               message VARCHAR(50),
-                               status VARCHAR(50),
-                               foreign key (order_id) references purchase_order(order_id)
+CREATE TABLE customer_payment (
+                                  payment_id uuid default random_uuid() primary key,
+                                  order_id uuid,
+                                  customer_id int,
+                                  status VARCHAR(50),
+                                  amount int,
+                                  foreign key (customer_id) references customer(id)
 );
 
-CREATE TABLE order_inventory (
-                                 id int AUTO_INCREMENT primary key,
-                                 order_id uuid unique,
-                                 inventory_id uuid,
-                                 success boolean,
-                                 status VARCHAR(50),
-                                 message VARCHAR(50),
-                                 foreign key (order_id) references purchase_order(order_id)
-);
+insert into customer(name, balance)
+values
+    ('sam', 100),
+    ('mike', 100),
+    ('john', 100);
